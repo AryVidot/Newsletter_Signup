f64e153189c46fc3beb35580c5f7a6a267dbe5fa
Ary Vidot <aryvid@gmail.com>
2020-07-27 21:09:21 +0100

    app.js updtaed

--- app.js
+++ app.js
@@ -1,12 +1,62 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
 
 const app = express();
+app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set up the route
 app.get("/", function (req, res) {
-  res.send("Server is up and running again.");
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us17.api.mailchimp.com/3.0/list/664b32c8fa";
+  const options = {
+    method: "POST",
+    auth: "ary1:f0ecdba0c335f5cf6c4f3719ad44ec79-us17",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function () {
   console.log("Server is runnning on port 3000.");
 });
+
+// Mailchimp
+// API key
+// f0ecdba0c335f5cf6c4f3719ad44ec79-us17
+//Audience/List Id
+// 664b32c8fa
+
+// --data '{"name":"Freddie'\''s Favorite Hats","contact":{"company":"Mailchimp","address1":"675 Ponce De Leon Ave NE","address2":"Suite 5000","city":"Atlanta","state":"GA","zip":"30308","country":"US","phone":""},"permission_reminder":"You'\''re receiving this email because you signed up for updates about Freddie'\''s newest hats.","campaign_defaults":{"from_name":"Freddie","from_email":"freddie@freddiehats.com","subject":"","language":"en"},"email_type_option":true}' \
+// -
